28940492f332d60c68bfd82b346295cd2b190d8b
cuijianbina <871689293@qq.com>
2019-07-04 15:33:31 +0800

    读取yml配置文件中的信息

--- src/main/java/com/trs/shiro/controller/UserController.java
+++ src/main/java/com/trs/shiro/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.trs.shiro.controller;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
@@ -12,7 +11,6 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.trs.shiro.domain.User;
 
--- src/main/java/com/trs/shiro/domain/User.java
+++ src/main/java/com/trs/shiro/domain/User.java
@@ -6,6 +6,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import lombok.Data;
 
 @Data
@@ -16,9 +19,18 @@ public class User {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Expose
+	@SerializedName("newname")
 	private String name;
 	
 	private String password;
 	
 	private Integer status;
+
+	@Override
+	public String toString() {
+		return "{\"id\":\""+id+"\",\"name\":\""+ name+"\"}";
+	}
+	
+	
 }
